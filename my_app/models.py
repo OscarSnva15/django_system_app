d5f1a93926c1761463bb360bda84a30879019402
Oscar Suarez Nava <81348068+OscarSnva15@users.noreply.github.com>
2023-10-04 11:55:24 -0600

    creacion database models with django

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# los modelos sireven para crear tablas provienentes de una base de datos 
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    
+class Task(models.Model):
+    tittle = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,8 @@
 #django tiene mucho modulos, que podemos utilizar
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here. send files html
-
-
 def hello(request):
     return HttpResponse("<h2>Hello Word</h2>")
 
